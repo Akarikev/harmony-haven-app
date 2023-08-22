08aa8205a73aca6d8ba8d3454fd7913d93f19f12
Akarikev <princeelorm17@gmail.com>
2023-08-22 19:03:31 +0000

    mod: font fix on login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -51,7 +51,7 @@ export default function AuthenticationPage() {
               strokeWidth="2"
               strokeLinecap="round"
               strokeLinejoin="round"
-              className="mr-2 h-6 w-6 font-[inter]"
+              className="mr-2 h-6 w-6 font-sans"
             >
               <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
             </svg>
@@ -59,7 +59,7 @@ export default function AuthenticationPage() {
           </div>
           <div className="relative z-20 mt-auto">
             <blockquote className="space-y-2">
-              <p className="text-lg font-[inter]">
+              <p className="text-lg font-sans">
                 &ldquo;Harmony Haven saved me from my depression and anxiety its
                 a better application for Ghanaians&rdquo;
               </p>
@@ -67,13 +67,13 @@ export default function AuthenticationPage() {
             </blockquote>
           </div>
         </div>
-        <div className="lg:p-8 font-[inter]">
+        <div className="lg:p-8 font-sans">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
-              <h1 className="text-2xl font-semibold tracking-tight font-[inter]">
+              <h1 className="text-2xl font-semibold tracking-tight font-sans">
                 Create an account
               </h1>
-              <p className="text-sm text-muted-foreground">
+              <p className="text-sm text-muted-foreground ">
                 Enter your email below to create your account
               </p>
             </div>
